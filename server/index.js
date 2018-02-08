cb92f006e234c86326237529adc97f1d92567237
Alfonso <alfonmusan14@gmail.com>
2018-02-08 09:06:14 +0100

    Preguntar Jose tratar msg

--- server/index.js
+++ server/index.js
@@ -8,16 +8,26 @@ app.get('/', function (req, res) {
 });
 let socketIO = require('socket.io');
 let io = socketIO(server);
+let array_users = []
 const port = process.env.PORT || 3000;
 
 io.on('connection', (socket) => {
   console.log('user connected');
   socket.on('new_user', function (par) {
-    console.log(par)
+    console.log(array_users)
+    if (array_users.includes(par.usuario)) {
+      io.emit('error_new_user', 'El nombre de usuario está pillado')
+      console.log('error')
+    }
+    else {
+      console.log('correct')
+      io.emit('usuario_correcto', "El usuario se ha registrado correctamente")
+      array_users.push(par.usuario)
+    }
   })
-
-
 });
+
+
 server.listen(port, () => {
   console.log(`started on port: ${port}`);
 });
